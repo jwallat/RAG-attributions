f5fa5a744151ea5a546f28a2cb1b98fac0074513
Jonas Wallat <jwallat@hotmail.de>
2024-12-18 09:56:32 +0100

    Added missing run script

---
+++ advanced_rag.py
@@ -0,0 +1,172 @@
+from tqdm import tqdm
+from typing import Optional, List
+import datasets
+import wandb
+import time
+import json
+from langchain.docstore.document import Document as LangchainDocument
+from ragatouille import RAGPretrainedModel
+
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+
+from transformers import pipeline
+
+import logging
+import hydra
+from omegaconf import DictConfig
+
+from src.retriever import BM25Retriever, FaissRetriever, Retriever
+from src.llm import data_generator, get_reader_LLM
+from src.models import LLMReader
+from src.reranker import get_reranker
+from src.vector_store import get_vector_store
+
+
+log = logging.getLogger(__name__)
+
+
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig) -> None:
+    
+    log.info("Starting up...")
+
+    run_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+    log.info(f"Run dir: {run_dir}")
+
+    # Do not sync the wandb run if we are running on the cluster
+    wandb.init(project=cfg.wandb.project_name, config=dict(cfg), dir=run_dir, mode=cfg.wandb.mode)
+
+    if cfg.retriever.type == "faiss":
+        retriever = FaissRetriever(cfg.retriever)
+        # vector_store, embedding_model = get_vector_store(cfg)
+    elif cfg.retriever.type == "bm25":
+        retriever = BM25Retriever(cfg.retriever)
+    reranker = get_reranker(cfg)
+    llm = get_reader_LLM(cfg)
+
+
+    # Get the task dataset
+    task_ds = get_QA_dataset(cfg.dataset.name)
+
+    process_task_ds(
+        task_ds,
+        llm,
+        run_dir,
+        retriever,
+        reranker,
+    )
+
+
+def get_QA_dataset(ds_name):
+    task_ds = []
+
+    if ds_name == "wiki":
+        dataset = datasets.load_dataset("kilt_tasks", name="nq", split="test")
+        for row in dataset:
+            task_ds.append(
+                {
+                    "id": row["id"],
+                    "question": row["input"],
+                    "answer": row["output"],
+                }
+            )
+
+    elif ds_name == "squad":
+        dataset = datasets.load_dataset("rajpurkar/squad_v2", split="validation[:2]")
+        for row in dataset:
+            task_ds.append(
+                {
+                    "id": row["id"],
+                    "question": row["question"],
+                    "gold_answer": row["answers"],
+                }
+            )
+
+    return task_ds
+
+def process_task_ds(
+    dataset: List[dict],
+    llm: LLMReader,
+    run_dir: str,
+    retriever: Retriever,
+    reranker: Optional[RAGPretrainedModel] = None,
+    num_retrieved_docs: int = 30,
+    num_docs_final: int = 5,
+):
+
+    if False:
+        # Faiss Retriever
+        # TODO: needs to be put into the retriever class
+        log.info("=> Embedd queries...")
+        start_time = time.time()
+
+        questions = [row["question"] for row in dataset]
+        question_embeddings = embedding_model.embed_documents(questions)
+        end_time = time.time()
+        log.info(f"Embedding queries took: {end_time - start_time} seconds")
+
+        log.info("=> Retrieving documents...")
+        i = 0
+        for row in tqdm(dataset, desc="Retrieving documents"):
+            retrieved_docs = knowledge_index.similarity_search_by_vector(
+                question_embeddings[i], k=num_retrieved_docs
+            )
+            retrieved_docs_content = [
+                doc.page_content for doc in retrieved_docs
+            ]  # Keep only the text
+            row["retrieved_docs"] = retrieved_docs_content
+            i += 1
+
+    log.info("=> Retrieving documents...")
+
+    questions = [row["question"] for row in dataset]
+
+    i = 0
+    for row in tqdm(dataset, desc="Retrieving documents"):
+        retrieved_docs = retriever.retrieve(
+            questions[i], k=num_retrieved_docs
+        )
+        row["retrieved_docs"] = retrieved_docs
+        i += 1
+    
+
+    if reranker:
+        log.info("=> Reranking documents...")
+        for row in tqdm(dataset, desc="Reranking documents"):
+            reranked_docs = reranker.rerank(
+                row["question"], row["retrieved_docs"], k=num_docs_final
+            )
+            reranked_docs_content = [doc["content"] for doc in reranked_docs]
+
+            row["selected_docs"] = reranked_docs_content[:num_docs_final]
+    else:
+        for row in tqdm(dataset):
+            row["selected_docs"] = row["retrieved_docs"][:num_docs_final]
+
+    data_gen = data_generator(llm, dataset)
+
+    llm_pipeline = llm.load_model_pipeline()
+
+    index = 0
+    with tqdm(total=len(dataset), desc="LLM inference") as t:
+        for pipe_outs in llm_pipeline(data_gen):
+            for out in pipe_outs:
+                answer = out["generated_text"]
+
+                dataset[index]["answer"] = answer
+
+                t.update(1)
+                index += 1
+
+    # Save the results to file
+    with open(f"{run_dir}/results.jsonl", "w") as f:
+        # f.write("[")
+        for item in dataset:
+            f.write(json.dumps(item) + "\n")
+        # f.write("]")
+
+
+if __name__ == "__main__":
+    main()
--- grounded_gen.py
+++ grounded_gen.py
@@ -96,7 +96,8 @@ system_prompt = xml_system.format(context=format_docs_xml(documents))
 import transformers
 import torch
 
-model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+# model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+model_id = "princeton-nlp/gemma-2-9b-it-SimPO"
 
 pipeline = transformers.pipeline(
     "text-generation",
@@ -193,3 +194,30 @@ print(parser.invoke(xml_out))
 
 # %% [markdown]
 # #### LLM Post-Processing
+
+# %%
+# pip install 'transformers>=4.39.1'
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+
+quant_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.bfloat16)
+
+model_id = "CohereForAI/c4ai-command-r-plus-08-2024"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quant_config)
+
+# Format message with the command-r-plus-08-2024 chat template
+messages = [{"role": "user", "content": "Hello, how are you?"}]
+input_ids = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+input_ids = input_ids.to(model.device)
+## <BOS_TOKEN><|START_OF_TURN_TOKEN|><|USER_TOKEN|>Hello, how are you?<|END_OF_TURN_TOKEN|><|START_OF_TURN_TOKEN|><|CHATBOT_TOKEN|>
+
+gen_tokens = model.generate(
+    input_ids, 
+    max_new_tokens=100, 
+    do_sample=True, 
+    temperature=0.3,
+    )
+
+gen_text = tokenizer.decode(gen_tokens[0])
+print(gen_text)
+
