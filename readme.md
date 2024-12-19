7e177dc937b7daefb8be02a304f90e6fdb39e5f0
Jonas Wallat <jwallat@hotmail.de>
2024-12-19 10:59:57 +0100

    Refactoring vector store since parts are also needed for the pyserini index

--- readme.md
+++ readme.md
@@ -1,41 +1,49 @@
 # RAG Attributions
 
-Goal: 
-<br>"Certify" attributions -> guarantee correctness
+Goal:
+"Certify" attributions -> guarantee correctness
 
 Subgoals:
-<br>
+
 - Measure how many attributions are currently wrong
 - Understand whether models produce attributions in a post-hoc manner by looking for retrieved documents that best match internal belives
 - Test to show that the document is actually the reason for an attribution (=perturbations)
 
 ## Setup
-You will want to run this on a big GPU, we are currently using [Command-R+](https://huggingface.co/CohereForAI/c4ai-command-r-plus-4bit), which even in 4bit quantization requires a >40GB GPU. 
+
+You will want to run this on a big GPU, we are currently using [Command-R+](https://huggingface.co/CohereForAI/c4ai-command-r-plus-4bit), which even in 4bit quantization requires a >40GB GPU.
 
 ### Datasets
+
 We use the [KILT wikipedia snapshot](https://huggingface.co/datasets/facebook/kilt_wikipedia), which has corresponding open-domain QA datasets - of which we currently use [NaturalQuestions](https://huggingface.co/datasets/facebook/kilt_tasks). In both cases, we load them using the huggingface datasets package. So no manual downloading required ;)
 
 ### Environment
+
 Setup conda and install requirements
-```
+
+```bash
 conda create -n RAG python=3.10
 conda activate RAG
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
 ```
 
-
 ## Slurm and Execution Tips
+
 ### Interactive mode
-```
+
+```bash
 srun -t 30:00 --gpus=0 --cpus-per-task=4 /home/wallat/.conda/envs/RAG/bin/python advanced_rag.py -i
 ```
 
 ### Change hydra config
+
 This project uses hydra to manage the configurations. You can find the config files in the `/conf` folder. If you want to overwrite the default values, you can use the following notation:
-```
+
+```bash
 python advanced_rag.py ++dataset.name="squad"
 ```
 
 ### Quick test runs
-Since working on the entire wikipedia corpus takes quite some time, you may switch the dataset.name config to "squad", which then uses the squad dataset instead of wikipedia. 
+
+Since working on the entire wikipedia corpus takes quite some time, you may switch the dataset.name config to "squad", which then uses the squad dataset instead of wikipedia.
--- src/vector_store.py
+++ src/vector_store.py
@@ -1,23 +1,14 @@
-from langchain_text_splitters import TokenTextSplitter
-from tqdm import tqdm
-from typing import Optional, List, Tuple
-import datasets
-
-from langchain.docstore.document import Document as LangchainDocument
-from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
-from transformers import AutoTokenizer
-from transformers import AutoTokenizer
-
+from typing import Tuple
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores.utils import DistanceStrategy
-
 import torch
-from transformers import AutoTokenizer
-
 import logging
 from omegaconf import DictConfig
 import time
+
+from src.dataset import prepare_data
+
 
 log = logging.getLogger(__name__)
 
@@ -45,144 +36,11 @@ def get_vector_store(cfg: DictConfig) -> Tuple[FAISS, HuggingFaceEmbeddings]:
 
             # docs_processed = prepare_data(cfg)
             # vector_store, _ = get_document_store(cfg, docs_processed)
-    
+
     docs_processed = prepare_data(cfg)
     vector_store, _ = get_document_store(cfg, docs_processed)
 
     return vector_store, embedding_model
-
-def prepare_data(cfg: DictConfig) -> List[LangchainDocument]:
-
-    if not cfg.dataset.use_cached_preprocessed_dataset:
-
-        if not cfg.dataset.use_cached_raw_dataset:
-
-            log.info("Preapring data:")
-
-            if cfg.dataset.name == "wiki":
-                dataset = datasets.load_dataset("kilt_wikipedia", split="full")
-                log.info(f"Dataset has so many items: {len(dataset)}")
-
-                RAW_KNOWLEDGE_BASE = [
-                    LangchainDocument(
-                        page_content="".join(line for line in doc["text"]["paragraph"]),
-                        metadata={
-                            "anchors": doc["anchors"],
-                            "wikidata_info": doc["wikidata_info"],
-                            "kilt_id": doc["kilt_id"],
-                            "wikipedia_id": doc["wikipedia_id"],
-                            "wikipedia_title": doc["wikipedia_title"],
-                        },
-                    )
-                    for doc in tqdm(dataset)
-                ]
-
-            elif cfg.dataset.name == "squad":
-                # Squad v2
-                # dataset = datasets.load_dataset("rajpurkar/squad_v2", split="train")
-                dataset = datasets.load_dataset("rajpurkar/squad_v2", split="train[:10]")
-                RAW_KNOWLEDGE_BASE = [
-                    LangchainDocument(
-                        page_content=doc["context"], metadata={"source": doc["id"]}
-                    )
-                    for doc in tqdm(dataset)
-                ]
-
-            del dataset
-            log.info(f"So many docs in KB: {len(RAW_KNOWLEDGE_BASE)}")
-
-            torch.save(
-                RAW_KNOWLEDGE_BASE, f"/home/wallat/RAG/data/faiss/{cfg.dataset.name}/RAW_docs.pt"
-            )
-
-        else:
-            log.info("Started loading the preprocessed documents. This may take a while...")
-            RAW_KNOWLEDGE_BASE = torch.load(
-                f"/home/wallat/RAG/data/faiss/{cfg.dataset.name}/RAW_docs.pt"
-            )
-
-        log.info("Before splitting my documents")
-
-        docs_processed = split_my_documents(
-            100,  # We choose a chunk size adapted to our model
-            RAW_KNOWLEDGE_BASE,
-            tokenizer_name=cfg.retriever.embedding_model_name,
-            cfg=cfg,
-        )
-
-        torch.save(
-            docs_processed, f"/home/wallat/RAG/data/faiss/{cfg.dataset.name}/split_docs.pt"
-        )
-    
-    else:
-        docs_processed = torch.load(f"/home/wallat/RAG/data/faiss/{cfg.dataset.name}/split_docs.pt")
-
-    return docs_processed
-
-
-def split_my_documents(
-    chunk_size: int,
-    knowledge_base: List[LangchainDocument],
-    tokenizer_name: str,
-    cfg: DictConfig,
-) -> List[LangchainDocument]:
-    """
-    Split documents into chunks of maximum size `chunk_size` tokens and return a list of documents.
-    """
-    # text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
-    #     AutoTokenizer.from_pretrained(tokenizer_name),
-    #     chunk_size=chunk_size,
-    #     chunk_overlap=0,
-    #     add_start_index=True,
-    #     strip_whitespace=True,
-    #     # separators=MARKDOWN_SEPARATORS,
-    # )
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_fast=True)
-    text_splitter = TokenTextSplitter.from_huggingface_tokenizer(
-        tokenizer,
-        chunk_size=chunk_size,
-        chunk_overlap=0,
-        add_start_index=True,
-        strip_whitespace=True,
-    )
-
-    log.info("Splitting documents using the hierarchical character splitter...")
-    docs_processed = []
-    for doc in tqdm(knowledge_base):
-        # log.info(f"\n\n\n\nHere is the full doc: {doc.page_content}")
-        split_docs = text_splitter.split_documents([doc])
-
-        # log.info(f"\nThere are {len(split_docs)} split docs")
-        # log.info(f"\n\nHere is the split doc: {split_docs[0]}")
-
-        for split_doc in split_docs:
-            if "wikipedia_title" in doc.metadata:
-                split_doc.page_content = (
-                    doc.metadata["wikipedia_title"] + "\n" + split_doc.page_content
-                )
-            # log.info(f"\n\nHere is a split doc: {split_doc.page_content}")
-            # log.info(f"\nWith lengths: {len(tokenizer.tokenize(split_doc.page_content))}")
-            docs_processed.append(split_doc)
-
-    log.info("Done splitting documents.")
-
-    log.info("Removing duplicates...")
-
-    # Remove duplicates
-    unique_texts = {}
-    docs_processed_unique = []
-    for doc in tqdm(docs_processed):
-        if doc.page_content not in unique_texts:
-            unique_texts[doc.page_content] = True
-            docs_processed_unique.append(doc)
-
-    log.info("Done removing duplicates.")
-
-    log.info("\n\nHere are 2 example documents:")
-    log.info(docs_processed_unique[0])
-    log.info(docs_processed_unique[42])
-
-    return docs_processed_unique
 
 
 def get_embedding_model(embedding_model_name: str) -> HuggingFaceEmbeddings:
@@ -203,10 +61,14 @@ def get_embedding_model(embedding_model_name: str) -> HuggingFaceEmbeddings:
 
 
 def get_document_store(cfg, docs_processed) -> FAISS:
-    embedding_model = get_embedding_model(embedding_model_name=cfg.retriever.embedding_model_name)
+    embedding_model = get_embedding_model(
+        embedding_model_name=cfg.retriever.embedding_model_name
+    )
     log.info("Starting to ingest documents into FAISS database...")
 
-    vector_store = FAISS.from_documents(docs_processed, embedding_model, distance_strategy=DistanceStrategy.COSINE)
+    vector_store = FAISS.from_documents(
+        docs_processed, embedding_model, distance_strategy=DistanceStrategy.COSINE
+    )
     log.info("Done ingesting documents into FAISS database")
 
     vector_store.save_local(f"/home/wallat/RAG/data/faiss/{cfg.dataset.name}/")
